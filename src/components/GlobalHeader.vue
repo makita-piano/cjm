4fcd54c5c1db05366d7abfac81e1ce7bb29de310
makita-piano <hexyzaiza@gmail.com>
2020-10-12 21:40:09 +0900

    header fix

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-    <div class="grid display-none-mobile menu">
+    <div class="grid display-none-mobile">
       <div class="col-4">
         <RouterLink to="/" id="home" style="z-index: 9999">
-          <img src="tekitou.png" width="60px" height="100px" style="background-color: white;border-radius: 3rem;padding: .3rem;margin: .5rem 0;vertical-align: middle;box-sizing: content-box" alt="piano">
+          <img src="tekitou.png" width="80%" height="100px" style="background-color: white;border-radius: 3rem;padding: .3rem;margin: .5rem 0;vertical-align: middle;box-sizing: content-box" alt="piano">
         </RouterLink>
       </div>
-      <div class="grid col-8">
+      <div class="grid col-8" style="padding: 0 1rem">
         <ul class="grid col-12">
           <li class="col-middle">
             <RouterLink to="/philosophy">
