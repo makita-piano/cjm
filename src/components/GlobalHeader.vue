e206170a1809bf673f739dfb5cd29f8e3f788af1
makita-piano <hexyzaiza@gmail.com>
2020-10-12 21:24:27 +0900

    header height add

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -3,32 +3,32 @@
     <div class="grid display-none-mobile menu">
       <div class="col-4">
         <RouterLink to="/" id="home" style="z-index: 9999">
-          <img src="music_piano.png" width="48px" height="48px" style="background-color: white;border-radius: 3rem;padding: .3rem;margin: .5rem 0;vertical-align: middle;box-sizing: content-box" alt="piano">
+          <img src="music_piano.png" width="60px" height="100px" style="background-color: white;border-radius: 3rem;padding: .3rem;margin: .5rem 0;vertical-align: middle;box-sizing: content-box" alt="piano">
         </RouterLink>
       </div>
       <div class="grid col-8">
         <ul class="grid col-12">
-          <li class="col">
+          <li class="col-middle">
             <RouterLink to="/philosophy">
               理念・特長
             </RouterLink>
           </li>
-          <li class="col">
+          <li class="col-middle">
             <RouterLink to="/lesson">
               レッスン
             </RouterLink>
           </li>
-          <li class="col">
+          <li class="col-middle">
             <RouterLink to="/event">
               講座・イベント
             </RouterLink>
           </li>
-          <li class="col">
+          <li class="col-middle">
             <RouterLink to="/rental">
               スタジオ貸出
             </RouterLink>
           </li>
-          <li class="col">
+          <li class="col-middle">
             <RouterLink to="/contact">
               お問い合わせ
             </RouterLink>
@@ -67,3 +67,5 @@
     </nav>
   </header>
 </template>
+
+<style>
