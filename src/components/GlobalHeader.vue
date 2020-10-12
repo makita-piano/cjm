e5b25d13c9fb43c188089e6ae05545674ec82762
makita-piano <hexyzaiza@gmail.com>
2020-10-12 21:32:24 +0900

    css setup

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -3,7 +3,7 @@
     <div class="grid display-none-mobile menu">
       <div class="col-4">
         <RouterLink to="/" id="home" style="z-index: 9999">
-          <img src="music_piano.png" width="60px" height="100px" style="background-color: white;border-radius: 3rem;padding: .3rem;margin: .5rem 0;vertical-align: middle;box-sizing: content-box" alt="piano">
+          <img src="tekitou.png" width="60px" height="100px" style="background-color: white;border-radius: 3rem;padding: .3rem;margin: .5rem 0;vertical-align: middle;box-sizing: content-box" alt="piano">
         </RouterLink>
       </div>
       <div class="grid col-8">
