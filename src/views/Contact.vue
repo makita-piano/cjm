8bf271450d010ff8844ba27874217a26578b7c49
makita-piano <hexyzaiza@gmail.com>
2020-10-15 16:11:39 +0900

    etc page create

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,28 +1,14 @@
 <template>
   <v-content>
-<div class="col-12">
-    <div class="title">
-        お問い合わせ
-    </div>
-    <div class="ancher">
-        <RouterLink to="/">
-            トップページ
-        </RouterLink>
-         > お問い合わせ
-    </div>
-</div>
-<div style="text-align: center;padding: 1rem;background-color: #eee">
-　お問い合わせは以下のアドレスにお送りください。<br>
-  mail@ykpianoforte.com
-</div>
+    準備中
   </v-content>
 </template>
 
 <script>
   export default {
     mounted(){
-      const title = "Contact | 加々見 茉耶"
-      const description = "加々見 茉耶 official pageのお問合せページです。兵庫県生まれ。5歳よりピアノを始める。 兵庫県立西宮高等学校音楽科を卒業し、東京藝術大学音楽学部器楽科ピアノ専攻に入学。第1回 ロザリオ・マルチアーノ国際ピアノコンクール（ウィーン）第2位。併せてシューベルト賞を受賞。"
+      const title = "お問い合わせ｜神宮の杜音楽院"
+      const description = "xxx "
       document.title = title
       document.querySelector("meta[property='og:title']")
       　.setAttribute('content', title)
--- src/views/Rental.vue
+++ src/views/Rental.vue
@@ -1,40 +1,93 @@
 <template>
   <v-content>
-<div class="col-12">
-            <div class="title">
-                プロフィール
-            </div>
-            <div class="ancher">
-        <RouterLink to="/">
-            トップページ
-        </RouterLink>
-                > プロフィール
-            </div>
-        </div>
-        <div class="grid col-12 profile">
-            <div class="col-12" style="text-align: center">
-                <img id="top-jpg" src="kagamimaya.jpg" style="padding: 1rem;width: 100%;box-sizing: border-box">
-            </div>
-            <div class="col-12" style="text-align: center;font-weight: bold;font-size: 1.3rem;margin-top: 2rem">
-                加々見 茉耶
-            </div>
-            <div class="col-12" style="text-align: center;font-weight: bold">
-                かがみ まや
-            </div>
-            <div class="col-12 text" style="margin-top: 2rem;padding: 1rem;margin-bottom: 2rem">
-            兵庫県生まれ。5歳よりピアノを始める。 兵庫県立西宮高等学校音楽科を卒業し、東京藝術大学音楽学部器楽科ピアノ専攻に入学。<br>
-第1回 ロザリオ・マルチアーノ国際ピアノコンクール（ウィーン）第2位。併せてシューベルト賞を受賞。 2011年、東京国際芸術協会主催の海外音楽大学派遣オーディションに合格し、渡航費・受講費全額免除でモスクワ音楽院のセミナーを受講。2014年、イタリア・イモラ音楽院の夏期講習会にてディプロマ取得。第2回イモラ国際ピアノアワード本選会にて奨学金を得る。東京藝術大学を卒業後、 2015年よりイタリア在住。第23回ペトロフピアノコンクール第1位。第23回 ジュリオ・ロスピリオージピアノコンクール第1位。優勝の褒賞として、イタリア国内で12のリサイタルをおこなう。メキシコ州立交響楽団の指揮者で芸術監督であるエンリケ・バティス氏に認められ、メキシコ州立交響楽団とプロコフィエフのピアノ協奏曲第2番を演奏。その模様は地元メディアにも取り上げられた。<br>
-これまでに、兵庫県立西宮高等学校オーケストラ、姫路交響楽団、Orchestra dell Concentus Musicus Patavinus、神戸フィルハーモニック、メキシコ州立交響楽団等と共演。芸大アーツイン丸の内、Casa Menotti 音楽フェスティバル、San Giacomo 音楽フェスティバル招聘アーティスト。在ドイツ日本大使館をはじめ、日本・ヨーロッパの各地各都市を中心にソロリサイタル出演多数。NPO法人イエローエンジェル奨学金を得てイモラ音楽院に在学し、レオニード・マルガリウス氏に師事。2019年イタリア最高位のディプロマを取得し卒業。
-            </div>
-        </div>
+    <div class="col-12">
+      <img src="tekitou.png" width="100%" height="300px" style="margin-bottom: 2rem" alt="piano">
+    </div>
+    <div class="grid col-12" style="padding: 1rem">
+      <div class="col-12" style="margin-bottom: 2rem">
+        CJMはあああああああああああああああああああああああああああああああああああああああああああああああああああ
+        ああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ<br>
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+      </div>
+      <h2 class="col-12">料金</h2>
+      <h3 class="col-12">利用料金　<span style="font-size: .8rem">※１時間あたり／あああああああああ</span></h3>
+      <ul class="col-12" style="font-size: .8rem">
+        <li>平日ああああああああああああああああああ　￥2,000</li>
+        <li>平日ああああああああああああああああああ　￥3,000</li>
+        <li>平日ああああああああああああああああああ　￥3,000</li><br>
+        <li>土日祝ああああああああああああああああああ　￥3,000</li>
+      </ul>
+      <h3 class="col-12">オプション　<span style="font-size: .8rem">※１時間あたり</span></h3>
+      <ul class="col-12" style="font-size: .8rem">
+        <li>スタインウェイ使用料　￥1,000</li>
+      </ul>
+      
+      <h3 class="col-12">会員特典　<span style="font-size: .8rem">※あああ</span></h3>
+      <ul class="col-12" style="font-size: .8rem">
+        <li>スタジオ利用料　20%OFF</li>
+      </ul>
+      
+      <h2 class="col-12">設備</h2>
+      <ul class="col-12" style="font-size: .8rem">
+        <li>ああああああああああああああああああ</li>
+        <li>ああああああああああああああああああ</li>
+        <li>ああああああああああああああああああ</li>
+        <li>ああああああああああああああああああ</li>
+      </ul>
+      
+      <h2 class="col-12">禁止事項</h2>
+      <ul class="col-12" style="font-size: .8rem">
+        <li>ああああああああああああああああああ</li>
+        <li>ああああああああああああああああああ</li>
+        <li>ああああああああああああああああああ</li>
+      </ul>
+      
+      <ul class="col-12" style="font-size: .8rem">
+        <li>ああああああああああああああああああ</li>
+        <li>ああああああああああああああああああ</li>
+      </ul>
+      
+      <h2 class="col-12">スタジオ予約</h2>
+      <div class="col-12" style="margin-bottom: 2rem;font-size: .8rem">
+        <a href="/">スタジオの予約はこちらから</a>
+      </div>
+      
+      
+      
+      
+      <div class="col-12" style="margin-bottom: 2rem;font-size: .8rem">
+        CJMはあああああああああああああああああああああああああああああああああああああああああああああああああああ
+        ああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ<br>
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+      </div>
+      <h2 class="col-12">利便性を考えた立地と充実の設備</h2>
+      <div class="col-12" style="margin-bottom: 2rem;font-size: .8rem">
+        CJMはあああああああああああああああああああああああああああああああああああああああああああああああああああ
+        ああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ<br>
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああ
+      </div>
+      
+      <div class="col-12" style="margin-bottom: 2rem;font-size: .8rem">
+        <a href="/">院長「ー」はこちら</a>
+      </div>
+    </div>
   </v-content>
 </template>
 
 <script>
   export default {
     mounted(){
-      const title = "Profile | 加々見 茉耶"
-      const description = "加々見 茉耶(かがみ まや)　兵庫県生まれ。5歳よりピアノを始める。 兵庫県立西宮高等学校音楽科を卒業し、東京藝術大学音楽学部器楽科ピアノ専攻に入学。第1回 ロザリオ・マルチアーノ国際ピアノコンクール（ウィーン）第2位。併せてシューベルト賞を受賞。"
+      const title = "スタジオ貸出｜神宮の杜音楽院"
+      const description = "xxx "
       document.title = title
       document.querySelector("meta[property='og:title']")
       　.setAttribute('content', title)
