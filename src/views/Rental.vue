4b627dd422c0ab423bbe873258fb421ab13060fc
makita-piano <hexyzaiza@gmail.com>
2020-10-18 17:41:25 +0900

    pre_list add

--- src/views/Rental.vue
+++ src/views/Rental.vue
@@ -25,14 +25,14 @@
       </div>
       <h2 id="c1" class="col-12">料金</h2>
       <h3 class="col-12">利用料金　<span style="font-size: .8rem">※1時間あたり / YAMAHA C3型 グランドピアノ 使用可能</span></h3>
-      <ul class="col-12" style="font-size: .8rem">
+      <ul class="col-12" style="font-size: .8rem;margin-bottom: 1.5rem">
         <li>平日 午前（9:00-13:00）　	￥2,000</li>
         <li>平日 午後（13:00-17:00）　	￥2,500</li>
         <li>平日 夜間（17:00-23:00）　	￥3,000</li><br>
         <li>土日祝日 （終日）　	￥3,000</li>
       </ul>
       <h3 class="col-12">オプション　<span style="font-size: .8rem">※１時間あたり</span></h3>
-      <ul class="col-12" style="font-size: .8rem">
+      <ul class="col-12" style="font-size: .8rem;margin-bottom: 1.5rem">
         <li>スタインウェイ使用料　￥1,000</li>
       </ul>
       
@@ -42,18 +42,18 @@
       </ul>
       
       <h2 id="c2" class="col-12">設備</h2>
-      <ul class="col-12" style="font-size: .8rem">
-        <li>Steinway & Sons D-274型 フルコンサートグランドピアノ 　（ドイツ・ハンブルク1996年製造）</li>
-        <li>YAMAHA C3型 グランドピアノ</li>
-        <li>自立譜面台　1本</li>
-        <li>椅子、机等有り</li>
+      <ul class="col-12 pre_list" style="font-size: .8rem;margin-bottom: 1.5rem">
+        <li>・Steinway & Sons D-274型 フルコンサートグランドピアノ 　（ドイツ・ハンブルク1996年製造）</li>
+        <li>・YAMAHA C3型 グランドピアノ</li>
+        <li>・自立譜面台　1本</li>
+        <li>・椅子、机等有り</li>
       </ul>
       
       <h2 id="c3" class="col-12">禁止事項</h2>
-      <ul class="col-12" style="font-size: .8rem">
-        <li>教室及びホールでのご飲食、喫煙</li>
-        <li>音楽関係以外の目的での使用</li>
-        <li>窓、扉、カーテンを開けての演奏</li>
+      <ul class="col-12 pre_list" style="font-size: .8rem;margin-bottom: 1.5rem">
+        <li>・教室及びホールでのご飲食、喫煙</li>
+        <li>・音楽関係以外の目的での使用</li>
+        <li>・窓、扉、カーテンを開けての演奏</li>
       </ul>
       
       <ul class="col-12" style="font-size: .8rem">
