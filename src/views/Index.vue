c623c7382afd7b01b038084f35406dda48912361
makita-piano <hexyzaiza@gmail.com>
2020-11-08 20:35:15 +0900

    tag news sort

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -61,7 +61,7 @@
 
     created(){
       axios
-        .get(process.env.VUE_APP_MICROCMS_URL + "/news?fields=publish_at,tag,title,title_url&limit=2",{
+        .get(process.env.VUE_APP_MICROCMS_URL + "/news?fields=publish_at,tag,title,title_url&orders=-publish_at&limit=2",{
           headers: { "X-API-KEY": process.env.VUE_APP_MICROCMS_KEY},
           data: {}
       })
