14348975f49edd5509adb1a6affb11535680c890
makita-piano <hexyzaiza@gmail.com>
2020-12-07 00:30:18 +0900

    notfound compornent is deleted

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -13,7 +13,7 @@
           {{ news.title }}
         </h2>
         <div class="col-12" style="text-align: right;color: #999">
-          {{date_formated(news.publishedAt)}}
+          {{date_formated(news.published_at)}}
         </div>
         <div class="col-12 news-contents" v-html="news.contents" style="padding: 1rem 0"></div>
       </div>
@@ -24,7 +24,6 @@
 <script>
   import axios from 'axios'
   import moment from "moment"
-  import Notfound from '../notfound'
 
   export default {
     name: "news-id",
@@ -40,12 +39,6 @@
             { hid: 'og:title', property: 'og:title', content: this.news.title + '｜神宮の杜音楽院' }
           ]
         }
-      }
-    },
-
-    data () {
-      return {
-        description: "",
       }
     },
     //filters=publish_at[exists]&
@@ -68,53 +61,6 @@
         moment.locale("ja");
         return moment(date).format("YYYY年MM月DD日（ddd）");
       }
-    },
-    components: {
-      Notfound
     }
   }
 </script>
-
-<style>
-  .loader,
-  .loader:after {
-    border-radius: 50%;
-    width: 10em;
-    height: 10em;
-  }
-  .loader {
-    margin: 60px auto;
-    font-size: 10px;
-    position: relative;
-    text-indent: -9999em;
-    border-top: 1.1em solid #999;
-    border-right: 1.1em solid #999;
-    border-bottom: 1.1em solid #999;
-    border-left: 1.1em solid #ffffff;
-    -webkit-transform: translateZ(0);
-    -ms-transform: translateZ(0);
-    transform: translateZ(0);
-    -webkit-animation: load8 1.1s infinite linear;
-    animation: load8 1.1s infinite linear;
-  }
-  @-webkit-keyframes load8 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-  @keyframes load8 {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-</style>
