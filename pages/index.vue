cad947ff497733ac15efda2bdae6fffa7fc87e31
makita-piano <hexyzaiza@gmail.com>
2020-12-20 21:41:11 +0900

    link add

--- pages/index.vue
+++ pages/index.vue
@@ -34,6 +34,37 @@
         </div>
         -->
         <h2 class="col-12">Links</h2>
+        <div class="grid col-12" style="margin-bottom: 2rem">
+          <div class="col-3_sm-6" style="padding: .5rem">
+            <a href="http://ykpianoforte.com/index.php" target="_blank">
+              <img src="~assets/img/banner_ykpianoforte.jpg" width="100%" style="border: 1px solid #999" alt="banner_ykpianoforte">
+            </a>
+          </div>
+          <div class="col-3_sm-6" style="padding: .5rem">
+            <a href="https://store.shopping.yahoo.co.jp/en-onlineshop/" target="_blank">
+              <img src="~assets/img/banner_onlineshop.jpg" width="100%" style="border: 1px solid #999" alt="banner_onlineshop">
+            </a>
+          </div>
+          <div class="col-3_sm-6" style="padding: .5rem">
+            <a href="https://kagamimaya.klavier.page/" target="_blank">
+              <img src="~assets/img/banner_mayakagami.jpg" width="100%" style="border: 1px solid #999" alt="banner_mayakagami">
+            </a>
+          </div>
+          <div class="grid col-3_sm-6 social" style="padding: .5rem">
+            <div class="grid col-12 twitter" style="position: relative">
+              <a href="https://twitter.com/cjmtokyo" target="_blank"  class="grid" style="position: absolute;top:45%">
+                <div class="col-1 social-icon" style="transform: translate(0%,-50%)">
+                  <img src="~assets/img/twitter_white.png">
+                </div>
+                <div class="col social-text" style="transform: translate(0%,-50%)">
+                  <div class="social-name">@cjmtokyo</div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+          
+          
         <div class="col-12" style="margin: .5rem 0 2rem 0;text-align: center">
           <iframe loading="lazy" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2Fcategory%2FMusic-Lessons---Instruction-School%2FCjm-Tokyo-%25E7%25A5%259E%25E5%25AE%25AE%25E3%2581%25AE%25E6%259D%259C%25E9%259F%25B3%25E6%25A5%25BD%25E9%2599%25A2-103707088022777%2F%22%20data-tabs%3D%22timeline%22%20data-width%3D%22%22%20data-height%3D%22%22%20data-small-header%3D%22false%22%20data-adapt-container-width%3D%22true%22%20data-hide-cover%3D%22false%22%20data-show-facepile%3D%22true%22%3E%3Cblockquote%20cite%3D%22https%3A%2F%2Fwww.facebook.com%2Fpages%2Fcategory%2FMusic-Lessons---Instruction-School%2FCjm-Tokyo-%25E7%25A5%259E%25E5%25AE%25AE%25E3%2581%25AE%25E6%259D%259C%25E9%259F%25B3%25E6%25A5%25BD%25E9%2599%25A2-103707088022777%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=160626627849426" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         </div>
